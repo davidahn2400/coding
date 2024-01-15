cd860f55685b15204295ccb60f0aa87c7d8bb0a6
DESKTOP-0LA73KJ\User <davidahn2400@naver.com>
2024-01-15 12:55:34 +0900

    아직 모르는 부분이 많다

--- 백준9일차/백준 1260번.py
+++ 백준9일차/백준 1260번.py
@@ -2,23 +2,23 @@ from collections import deque
 
 n, m, v = map(int, input().split())
 
-visited1 = [False]
-visited2 = [False]
+visited1 = [False] # dfs 그래프의 방문 여부를 담을 리스트
+visited2 = [False] # bfs 그래프의 방문 여부를 담을 리스트
 
-def dfs(graph, v, visited):
-  visited1[v] = True
-  print(v, end=' ')
+def dfs(graph, v, visited): 
+  visited1[v] = True # dfs 방문 처리
+  print(v, end=' ') # 방문 후, 정점 출력
   for i in graph[v]:
-    if not visited1[i]:
+    if not visited1[i]: # 방문 기록이 없다면
       dfs(graph, i, visited)
 
 def bfs(graph, start, visited):
   queue = deque(start)
-  visited[start] = True
-  while queue:
-    v = queue.popleft()
-    print(v, end=' ')
+  visited[start] = True # bfs 방문처리
+  while queue: # 더 이상 방문할 노드가 없을 때 까지
+    v = queue.popleft() # 제일 앞에 있는 노드 삭제
+    print(v, end=' ') # 정점 출력
     for i in graph[v]:
-      if not visited[i]:
-        queue.append(i)
-        visited[i] = True
+      if not visited[i]: # 방문 기록이 없다면
+        queue.append(i) # 노드 삽입
+        visited[i] = True # 방문처리
