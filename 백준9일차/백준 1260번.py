bb277e07425f62cfa90b94bb35ea7452b744b4b3
DESKTOP-0LA73KJ\User <davidahn2400@naver.com>
2024-01-16 20:33:34 +0900

    모르는 부분이 아직 많다

--- 백준9일차/백준 1260번.py
+++ 백준9일차/백준 1260번.py
@@ -1,24 +1,37 @@
 from collections import deque
 
-n, m, v = map(int, input().split())
+n, m, v = map(int, input().split()) # 정점, 간선수, 시작점을 입력받음
 
-visited1 = [False] # dfs 그래프의 방문 여부를 담을 리스트
-visited2 = [False] # bfs 그래프의 방문 여부를 담을 리스트
+graph = [[] for _ in range(n+1)]
 
-def dfs(graph, v, visited): 
+for i in range(m):
+  x, y = map(int, input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+  graph[x].sort()
+  graph[y].sort()
+
+visited1 = [False] * (n+1) # dfs 그래프의 방문 여부를 담을 리스트
+visited2 = [False] * (n+1) # bfs 그래프의 방문 여부를 담을 리스트
+
+def dfs(graph, v, visited1):  # dfs 함수 정의
   visited1[v] = True # dfs 방문 처리
   print(v, end=' ') # 방문 후, 정점 출력
-  for i in graph[v]:
-    if not visited1[i]: # 방문 기록이 없다면
-      dfs(graph, i, visited)
+  for i in graph[v]: # 방문기록이 없고 인덱스에 값이 있다면
+    if not visited1[i]: # 재귀함수로 호출될때마다 visited는 1이되고 v에 i 삽입
+      dfs(graph, i, visited1)
 
-def bfs(graph, start, visited):
-  queue = deque(start)
-  visited[start] = True # bfs 방문처리
+def bfs(graph, start, visited2): # bfs 함수 정의
+  queue = deque([start]) # Queue 구현을 위해 deque 라이브러리 사용
+  visited2[start] = True # bfs 방문처리
   while queue: # 더 이상 방문할 노드가 없을 때 까지
-    v = queue.popleft() # 제일 앞에 있는 노드 삭제
-    print(v, end=' ') # 정점 출력
-    for i in graph[v]:
-      if not visited[i]: # 방문 기록이 없다면
-        queue.append(i) # 노드 삽입
-        visited[i] = True # 방문처리
+    v = queue.popleft() # 큐에서 하나의 노드를 뽑기
+    print(v, end=' ') # 뽑은 노드 출력
+    for j in graph[v]: # 해당 노드와 연결되있는
+      if not visited2[j]: # 방문기록이 없고 인덱스에 값이 있다면
+        queue.append(j) # 큐에 삽입
+        visited2[j] = True 
+
+dfs(graph, v, visited1)
+print("")
+bfs(graph, v, visited2)
